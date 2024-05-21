c89c20fef66dbd1de234a10055a69406884c1c81
SalsaNP777 <salsa.nputr777@gmail.com>
2024-05-21 19:44:03 +0700

    finish

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) 
+    {
+        int n = 5, k = 0;
+
+        Console.WriteLine();
+
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if(k < n){
+                    k++;
+                }
+                else{
+                    k--;
+                }
+                Console.Write(k + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
